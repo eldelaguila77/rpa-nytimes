12ea316210dd915d9b8e20daef3874f3a8af73c6
Eduardo Ramírez <eduardo.ramirez@zubale.com>
2023-07-02 12:49:24 -0600

    master if images directory doesnot exist, it will be created

--- functions/save_picture_file.py
+++ functions/save_picture_file.py
@@ -5,6 +5,24 @@ import logging
 from functions.utils import path_to_save_files 
 
 
+def create_images_directory(output_path: str) -> None:
+    """
+    The function creates a directory named "images" at the specified output path.
+    
+    :param output_path: The `output_path` parameter is a string that represents the path where the
+    directory should be created
+    :type output_path: str
+    """
+    directory_name = "images"
+    path = os.path.join(output_path, directory_name)
+
+    try:
+        os.mkdir(path)
+    except FileExistsError:
+        logging.warning("The directory already exists")
+    except Exception as e:
+        logging.error("Error while directory was been created %s", str(e))
+
 def save_picture(image_uri: str, filename: str) -> None:
     """
     The `save_picture` function downloads an image from a given URI and saves it to a specified file
@@ -23,6 +41,8 @@ def save_picture(image_uri: str, filename: str) -> None:
     export_path = path_to_save_files()
     img_directory = os.path.join(export_path, 'images')
 
+    #Process
+    create_images_directory(export_path) 
     if image_uri != "N/A":
         img_data = requests.get(image_uri).content
         file_path = os.path.join(img_directory, filename)
